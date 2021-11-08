f2a0162acdc3e626ad60b8e96669bbf051a4725e
Jeffrey Zhang <jeffrey.zhang@me.com>
2021-11-08 21:17:08 +0800

    class demo code

---
+++ training_program_2021/SearchEngineBase.py
@@ -0,0 +1,45 @@
+class SearchEngineBase(object):
+    def __init__(self):
+        pass
+    
+    def add_corpus(self, file_path):
+        with open(file_path,'r') as fin:
+            text = fin.read()
+        self.process_corpus(file_path,text)
+        
+    def process_corpus(self, id, text):
+        raise Exception('process_corpus not implemented.')
+    
+    def search(self,query):
+        raise Exception('search not implemented.')
+    
+def main(search_engine):
+    for file_path in ['1.txt', '2.txt', '3.txt', '4.txt', '5.txt']:
+        search_engine.add_corpus('training_program_2021/' + file_path)
+        
+    while True:
+        query = input('please enter query word\n')
+        results = search_engine.search(query)
+        print('found {} results'.format(len(results)))
+        for result in results:
+            print(result)
+        
+        print('\n')
+            
+class SimpleEnginee(SearchEngineBase):
+    def __init__(self):
+        super(SimpleEnginee,self).__init__()
+        self.__id_to_texts = {}
+        
+    def process_corpus(self, id, text):
+        self.__id_to_texts[id] = text
+        
+    def search(self,query):
+        results = []
+        for id, text in self.__id_to_texts.items():
+            if query in text:    
+                results.append(id)  
+        return results
+    
+search_engine = SimpleEnginee()
+main(search_engine)
--- training_program_2021/codepilot.py
+++ training_program_2021/codepilot.py
@@ -1,1 +1,22 @@
-# find largest element from a list of elements
+# define a function to read ini file
+def read_ini(filename):
+    # open the file
+    with open(filename, 'r') as f:
+        # read the file
+        content = f.read()
+    # split the file into lines
+    lines = content.split('\n')
+    # define a dictionary to store the data
+    data = {}
+    # loop over the lines
+    for line in lines:
+        # split the line into words
+        words = line.split('=')
+        # if there are two words
+        if len(words) == 2:
+            # add the data to the dictionary
+            data[words[0]] = words[1]
+    # return the dictionary
+    return data
+
+# define a compare function to
