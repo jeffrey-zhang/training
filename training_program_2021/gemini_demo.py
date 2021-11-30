cdee3e2eacfa62518c5f2d233dc33f2efb9076e7
Jeffrey Zhang <jeffrey.zhang@me.com>
2021-11-30 09:11:18 +0800

    gemini demo

---
+++ training_program_2021/gemini_demo.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import requests
+
+periods = '3600'
+
+resp = requests.get('https://api.cryptowat.ch/markets/gemini/btcusd/ohlc', params={'periods': periods})
+data = resp.json()
+
+print(data)
+
+df = pd.DataFrame(
+    data['result'][periods],
+    columns=['CloseTime', 'OpenPrice', 'HighPrice', 'LowPrice', 'ClosePrice', 'Volume', 'NA'])
+
+print(df.head())
+
+df['ClosePrice'].plot(figsize=(14, 7))
+plt.show()
