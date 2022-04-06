1fb95419ddb9792dccb1566e5d86dd0e360330c6
Jeffrey Zhang <jeffrey_zhang@trendmicro.com>
2022-04-06 14:45:18 +0800

    add more views

--- training_program_2022/django-demo/mysite/polls/views.py
+++ training_program_2022/django-demo/mysite/polls/views.py
@@ -1,6 +1,17 @@
+from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at questions {}.".format(question_id))
+
+def results(request, question_id):
+    response = "You're looking at the results of question {}."
+    return HttpResponse(response.format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}.".format(question_id))
