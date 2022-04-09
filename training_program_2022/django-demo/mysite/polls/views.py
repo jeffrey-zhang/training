a8b32df2fab33937136703013a4dd0f8bdd098d3
Jeffrey Zhang <jeffrey_zhang@trendmicro.com>
2022-04-09 23:40:50 +0800

    finish the polls app

--- training_program_2022/django-demo/mysite/polls/views.py
+++ training_program_2022/django-demo/mysite/polls/views.py
@@ -1,6 +1,6 @@
 from urllib import response
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.template import loader
 from .models import Questions
 
@@ -14,7 +14,11 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at questions {}.".format(question_id))
+    try:
+        question = Questions.objects.get(pk=question_id)
+    except Questions.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question {}."
