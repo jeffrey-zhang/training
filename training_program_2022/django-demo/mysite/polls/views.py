7651787d0b212de9c3e1765a5f29d8f476d1d42c
Jeffrey Zhang <jeffrey_zhang@trendmicro.com>
2022-04-10 00:15:34 +0800

    finish results and vote page

--- training_program_2022/django-demo/mysite/polls/views.py
+++ training_program_2022/django-demo/mysite/polls/views.py
@@ -1,8 +1,9 @@
 from urllib import response
-from django.shortcuts import render
-from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Questions
+from django.urls import reverse
+from .models import Choice, Questions
 
 
 def index(request):
@@ -13,6 +14,7 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 def detail(request, question_id):
     try:
         question = Questions.objects.get(pk=question_id)
@@ -20,9 +22,22 @@ def detail(request, question_id):
         raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
 
+
 def results(request, question_id):
-    response = "You're looking at the results of question {}."
-    return HttpResponse(response.format(question_id))
+    question = get_object_or_404(Questions, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question {}.".format(question_id))
+    question = get_object_or_404(Questions, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You don't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
