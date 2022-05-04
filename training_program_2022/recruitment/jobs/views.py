2819b317134a8e2bbc3dfa2765b7ff10a6443ec0
Jeffrey Zhang <jeffrey_zhang@trendmicro.com>
2022-05-04 23:17:43 +0800

    optimize the code

--- training_program_2022/recruitment/jobs/views.py
+++ training_program_2022/recruitment/jobs/views.py
@@ -7,18 +7,21 @@ from jobs.models import Cities, Job, JobTypes
 
 def joblist(request):
     job_list = Job.objects.order_by('job_type')
-    
+
     # <1> 函数方式render 页面
-    template = loader.get_template('joblist.html') # <1>joblist.html 获取到template中
-    context = {'job_list': job_list} # <1>在html中定义了block content
+    # <1>joblist.html 获取到template中
+    template = loader.get_template('joblist.html')
+    context = {'job_list': job_list}  # <1>在html中定义了block content
 
     for job in job_list:
         job.job_city = Cities[job.job_city][1]
         job.job_type = JobTypes[job.job_type][1]
 
-    #return HttpResponse(template.render(context)) # <1> render函数将context中的内容渲染到template中
+    # return HttpResponse(template.render(context)) # <1> render函数将context中的内容渲染到template中
 
-    return render(request,'joblist.html',{'job_list':job_list}) # <2> 方法2 直接渲染页面
+    # <2> 方法2 直接渲染页面
+    return render(request, 'joblist.html', {'job_list': job_list})
+
 
 def detail(request, job_id):
     try:
@@ -26,5 +29,5 @@ def detail(request, job_id):
         job.job_city = Cities[job.job_city][1]
     except Job.DoesNotExist:
         raise Http404("Job does not exist")
-    
+
     return render(request, 'job.html', {'job': job})
