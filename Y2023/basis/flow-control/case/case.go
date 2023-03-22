3bc8ecced5b79bd0b363720b812f0054e86d7b8d
Jeffrey Zhang <jeffrey.zhang@macbook-air>
2023-03-22 15:55:43 +0800

    add flow-control demo

---
+++ Y2023/basis/flow-control/case/case.go
@@ -0,0 +1,33 @@
+package _case
+
+import "fmt"
+
+func FlowControlCase() {
+	ifElseCase(0)
+	ifElseCase(1)
+	ifElseCase(2)
+
+	forCase()
+}
+
+func ifElseCase(a int) {
+	if a == 0 {
+		fmt.Println("executed if")
+	} else if a == 1 {
+		fmt.Println("executed else if")
+	} else {
+		fmt.Println("executed else")
+	}
+}
+
+func forCase() {
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			continue
+		}
+		fmt.Println("forL i=", i)
+	}
+}
+
+// implement a function to integrated with MS AAD
+
